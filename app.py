1ed4c4a30362d63dbb19def8ba89cc73bdab6a5c
alexismaharaj <alexis.maharaj@ttigroupna.com>
2025-07-17 09:54:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -52,4 +52,7 @@ def redeem():
         return jsonify({"error": "Failed to expire pass", "details": response.text}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
